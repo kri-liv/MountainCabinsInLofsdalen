c48112821720c342d34000f8719ac7af709565b7
kristina-liv <kicki_lif@hotmail.com>
2020-09-25 10:10:45 +0200

    Added markers to google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,23 @@
 function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 3,
-        center: { lat: -28.024, lng: 140.887 }
+        zoom: 15,
+        center: { lat: 62.113482, lng: 13.296200 }
     });
 
     // Create an array of alphabetical characters used to label the markers.
     var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     var locations = [
-        { lat: -31.563910, lng: 147.154312 },
-        { lat: -33.718234, lng: 150.363181 },
-        { lat: -33.727111, lng: 150.371124 },
-        { lat: -43.999792, lng: 170.463352 }
+        { lat: 62.114080, lng: 13.294574 },
+        { lat: 62.112139, lng: 13.271385 },
+        { lat: 62.114006, lng: 13.252872 },
+        { lat: 62.113482, lng: 13.296200 },
+
+        { lat: 62.112852, lng: 13.271819 },
+        { lat: 62.113444, lng: 13.296156 },
+        { lat: 62.113614, lng: 13.296927 },
+        { lat: 62.114454, lng: 13.314653 },
     ]
 
     // Add some markers to the map.
